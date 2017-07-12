8be75fbe088034bbefc2d410bc8d711dfd34672e
LKW-PC\LKW <lkwong1013@gmail.com>
2017-07-13 00:58:07 +0800

    Dockerize script for pc

--- dockerize.bat
+++ dockerize.bat
@@ -1,9 +1,12 @@
 @echo off
-set CONTAINER_ID=iou
+set CONTAINER_ID=react-cms
 echo %CONTAINER_ID%
-call gradle clean war
-docker build -t %CONTAINER_ID% .
-docker stop %CONTAINER_ID%
-docker rm %CONTAINER_ID%
-docker run -p 8123:8080 --name %CONTAINER_ID% %CONTAINER_ID%
+cd script
+call npm run build
+cd ..
+call gradle clean myCopy war
+call docker build -t %CONTAINER_ID% .
+call docker stop %CONTAINER_ID%
+call docker rm %CONTAINER_ID%
+call docker run -p 8123:8080 --name %CONTAINER_ID% %CONTAINER_ID%
 
