b0c6d2cf6d510eb6d0f88a30e8cde6efb3a76eee
GTOMATO-HK\max.wong <max.wong@gtomato.com>
2017-07-13 15:31:47 +0800

    Dockerize added debug config

--- dockerize1.sh
+++ dockerize1.sh
@@ -2,6 +2,7 @@
 
 PROJECT_NAME="react-cms"
 HOST_PORT=8123
+DEBUG_PORT=8000
 MVN_OPT=
 
 # Compile frontend
@@ -12,4 +13,7 @@ gradle clean myCopy war
 docker build -t $PROJECT_NAME .
 docker stop $PROJECT_NAME
 docker rm $PROJECT_NAME
-docker run -p$HOST_PORT:8080 --name $PROJECT_NAME $PROJECT_NAME
+docker run  -p$HOST_PORT:8080 \
+            -p$DEBUG_PORT:$DEBUG_PORT \
+            -e "JAVA_OPTS=\"-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n\"" \
+            --name $PROJECT_NAME $PROJECT_NAME \
