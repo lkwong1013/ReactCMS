f41b2ddc5f7c5bf1e6b827dd26d903f0a07aca27
LKW-PC\LKW <lkwong1013@gmail.com>
2017-09-12 00:23:52 +0800

    Update bug fix to deployable

--- deploy.bat
+++ deploy.bat
@@ -6,3 +6,5 @@ call npm run build
 cd ..
 call gradle clean myCopy build
 java -jar build/libs/bootLesson-0.0.1-SNAPSHOT.jar
+
+#gradle bootRun --debug-jvm
--- src/main/java/com/example/controller/ApiAuthController.java
+++ src/main/java/com/example/controller/ApiAuthController.java
@@ -11,10 +11,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
